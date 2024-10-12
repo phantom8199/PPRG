9d37d98f5e890bac57490404cf19737f1ed054dd
phantom8199 <106472427+phantom8199@users.noreply.github.com>
2024-10-12 13:24:02 +0200

    Add files via upload

---
+++ PPRG 12.10.cpp
@@ -0,0 +1,106 @@
+
+//					ZADANIE 1.1
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    
+//    string imie, nr, uczelnia;
+//
+//    cout << "wypisywanie w osobnych liniach danych studenta." << endl;
+//    cout << "podaj swoje imie:  ";
+//    cin >> imie;
+//    cout << "podaj numer studenta: ";
+//    cin >> nr;
+//    cout << "oraz nazwe uczelni: ";
+//    cin >> uczelnia;
+//
+//
+//    cout << "twoje imie: " << imie << "\nnumer studenta: " << nr << "\nnazwa uczelni: " << uczelnia << endl;
+//
+//    return 0;
+//
+//
+//
+//    
+//}
+
+
+//					ZADANIE 1.2
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()	{
+//	int number;
+//
+//	cout << "Wprowadz liczbe calkowita: ";
+//	cin >> number;
+//	if (number > 0) {
+//		cout << "Wpisales dodatnia liczbe calkowita: " << number << endl;
+//
+//	}
+//	if (number < 0) {
+//		cout << "Wpisales ujemna liczbe calkowita:  " << number << endl;
+//
+//	}
+//	if (number == 0) {
+//		cout << "Wpisales zero ";
+//		return 0;
+//	}
+//}
+
+//					ZADANIE 1.3
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int number;
+//
+//	cout << "Wprowadz liczbe calkowita: ";
+//	cin >> number;
+//	if (number % 2 == 0 && number != 0) {
+//		cout << "Wpisales parzysta liczbe calkowita: " << number << endl;
+//
+//	}
+//	if (number % 2 != 0) {
+//		cout << "Wpisales nieparzysta liczbe calkowita:  " << number << endl;
+//
+//	}
+//	if (number == 0) {
+//		cout << "Wpisales zero ";
+//		return 0;
+//	}
+//}
+//					ZADANIE 1.4
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//    int a, b, c;
+//
+//    cout << "Znajdowanie najwiekszej liczby posrod trzech podanych" << endl;
+//    cout << "Podaj pierwsza liczbe:  ";
+//    cin >> a;
+//    cout << "Podaj druga liczbe: ";
+//    cin >> b;
+//    cout << "Podaj trzecia liczbe: ";
+//    cin >> c;
+//    if (a > b && a > c) {
+//        cout << "Liczba: " << a << " jest najwieksza";
+//    }
+//    if (b > a && b > c) {
+//        cout << "Liczba: " << b << " jest najwieksza";
+//    }
+//    if (c > a && c > b) {
+//        cout << "Liczba: " << c << " jest najwieksza";
+//    }
+//    return 0;
+//
+//
+//}
